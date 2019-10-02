45709124b99e780be8c5496e99da6a854bccae79
StevenJ <tonystevenj@gmail.com>
2019-10-01 22:00:23 -0400

    HomeWork version - All english comment

--- src/singlethreadFTP/MyFTP_Single.java
+++ src/singlethreadFTP/MyFTP_Single.java
@@ -11,8 +11,8 @@ import java.net.Socket;
  * open inet.cis.fiu.edu
  * user demo
  * pass demopass
- * 家里的网址： port 10,0,0,133,34,184
- * 学校网址： port 10,108,5,181,34,184
+ * port 10,0,0,133,34,184
+ * port 10,108,5,181,34,184
  * testUpload-HongjingWang.txt
  */
 public class MyFTP_Single {
@@ -29,22 +29,13 @@ public class MyFTP_Single {
 
     public void start() {
         this.brFromKeyboard = new BufferedReader(new InputStreamReader(System.in));
-//        TestConnect();
         getServerInfo();
-//        readFromServer();
-        /**一旦建立链接就先发一个设定字符集的数据*/
         send("OPTS UTF8 ON");
         readFromServer();
         while (true) {
             keyboardToServer();
-//            readFromServer();
         }
     }
-
-    /**
-     * 要求用户输入要链接的地址，这个方法就是用来获得inetAddress的
-     * 并没有实例化对象，实例化对象在initilize()里面
-     */
     private void getServerInfo() {
         while (true) {
             System.out.print("MyFTP> ");
@@ -52,19 +43,18 @@ public class MyFTP_Single {
             try {
                 userin = brFromKeyboard.readLine();
             } catch (IOException e) {
-                System.out.println("从用户输入读取数据失败");
+                System.out.println("Fail to read from user, please restart program");
                 e.printStackTrace();
             }
-            /**分隔键盘输入，查看指令 String[] userins*/
             String[] userins = userin.split(" ");
             if (!userins[0].equalsIgnoreCase("myftp")) {
-                System.out.println("指令不对");
+                System.out.println("Wrong command");
                 continue;
             }
             try {
                 this.inetAddress = InetAddress.getByName(userins[1]);
             } catch (Exception e) {
-                System.out.println("请输入有效服务器地址");
+                System.out.println("Can't find the address of this server. Please Re-type.");
                 continue;
             }
             initialize();
@@ -72,7 +62,6 @@ public class MyFTP_Single {
         }
         System.out.println("Please type in user name: ");
         String msg = "";
-        /**获得键盘输入 msg*/
         try {
             msg = brFromKeyboard.readLine();
         } catch (Exception e) {
@@ -93,9 +82,6 @@ public class MyFTP_Single {
         }
     }
 
-    /**
-     * 实例化要用到的对象类，链接服务器之后调用
-     */
     private void initialize() {
         try {
             this.client01 = new Socket(this.inetAddress, 21);
@@ -104,37 +90,16 @@ public class MyFTP_Single {
             this.is = client01.getInputStream();
             this.brFromServer = new BufferedReader(new InputStreamReader(this.is, "utf8"));
             this.localAddress = client01.getLocalAddress();
-//            this.dc = new DataChannel(portDataPort);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void TestConnect() {
-        try {
-            this.inetAddress = InetAddress.getByName("inet.cis.fiu.edu");
-            this.client01 = new Socket(this.inetAddress, 21);
-            System.out.println("Successful connected to server");
-            this.os = client01.getOutputStream();
-            this.is = client01.getInputStream();
-            this.brFromServer = new BufferedReader(new InputStreamReader(this.is, "utf8"));
-            this.localAddress = client01.getLocalAddress();
-//            this.dc = new DataChannel(portDataPort);
-            send("user demo");
-            readFromServer();
-            send("pass demopass");
-            readFromServer();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
 
 
     private void readFromServer() {
-        /**从服务器读取数据
-         * open speedtest.tele2.net
-         * */
         try {
             feedBack = brFromServer.readLine();
             System.out.println(feedBack);
@@ -148,7 +113,6 @@ public class MyFTP_Single {
 
     public void keyboardToServer() {
         String msg = "";
-        /**获得键盘输入 msg*/
         try {
             System.out.print("MyFTP> ");
             msg = brFromKeyboard.readLine();
@@ -161,11 +125,6 @@ public class MyFTP_Single {
             send(getPortInfo());
             readFromServer();
             send("nlst");
-            /*try {//证明了服务器有等待机制，并不是发一次指令然后发现门关着就回去了，它会等一哈儿
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }*/
             try {
                 new DataChannel(portDataPort).dataRead();
             } catch (IOException e) {
@@ -187,7 +146,6 @@ public class MyFTP_Single {
                         dc.getFile(msgs[1]);
                     }else {
                         System.out.println("File do not exist");
-//                        send("testMsg");
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -239,7 +197,6 @@ public class MyFTP_Single {
             send("XPWD");
             readFromServer();
         }else if(command.equalsIgnoreCase("user")){
-
             send(msg);
             readFromServer();
         }else if(command.equalsIgnoreCase("pass")){
@@ -257,7 +214,7 @@ public class MyFTP_Single {
             byte[] sends = msg.getBytes("utf8");
             os.write(sends);
         } catch (IOException e) {
-            System.out.println("os.write失败");
+            System.out.println("os.write failure");
             e.printStackTrace();
         }
     }
@@ -272,10 +229,9 @@ public class MyFTP_Single {
                 .append(",").append(msgs[3])
                 .append(",").append(num1)
                 .append(",").append(num2);
-        System.out.println("GET port 信息："+msg);
         return msg.toString();
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         MyFTP_Single ftp1 = new MyFTP_Single();
         ftp1.start();
     }
